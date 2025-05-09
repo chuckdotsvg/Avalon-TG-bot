e7ec014ff68ae19e643dbd202763f226180387c2
Carlo Aspren Gines Lara <101257700+chuckdotsvg@users.noreply.github.com>
2025-05-09 11:09:40 +0200

    refactor(rules): moved rules from text file to python file

---
+++ constants.py
@@ -0,0 +1,11 @@
+# each value of the key is a tuple:
+# 1st element is the mission team size
+# 2nd element is the number of good members
+playersToRules = {
+    5: ([2, 3, 2, 3, 3], 3),
+    6: ([2, 3, 4, 3, 4], 4),
+    7: ([2, 3, 3, 4, 4], 4),
+    8: ([3, 4, 4, 5, 5], 5),
+    9: ([3, 4, 4, 5, 5], 6),
+    10: ([3, 4, 4, 5, 5], 6),
+}
